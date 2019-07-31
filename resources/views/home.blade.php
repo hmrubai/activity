dec71db7f9b48f43d7278c770e74a083cf4013be
hmrubai <rubai.mobarak@gmail.com>
2019-08-01 05:13:55 +0600

    Done for today

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -218,11 +218,11 @@
                                                     <td>{{ $activity->approved_medicine_shop }}</td>
                                                   </tr>
                                                   <tr>
-                                                    <td>No of pharmacy completely ready to be approved as model pharmacy</td>
+                                                    <td>No. of pharmacy completely ready to be approved as model pharmacy</td>
                                                     <td>{{ $activity->no_of_pharmacy_completely_ready_to_be_approved_as_model_pharmacy }}</td>
                                                   </tr>
                                                   <tr>
-                                                    <td>No of pharmacy completely ready to be approved as medicine shop</td>
+                                                    <td>No. of pharmacy completely ready to be approved as medicine shop</td>
                                                     <td>{{ $activity->no_of_pharmacy_completely_ready_to_be_approved_as_medicine_shop }}</td>
                                                   </tr>
                                                   <tr>
@@ -230,20 +230,156 @@
                                                     <td>{{ $activity->value_of_seized_medicine }}</td>
                                                   </tr>
                                                 </table>
-                                                <li>
-                                                  <?php 
-                                                  if($activity->enforcement_information == "Mobile_Court"){
-                                                    ?> <img src="files/mobile_court/<?php echo $activity->mobile_court_case_paper; ?>" width="100%" alt="FingurePrint"  class="img-rounded user_image_shadow user-img-center"> <?php
-                                                  }elseif($activity->enforcement_information == "Magistrate_Court"){
-                                                    ?> <img src="files/magistrate_court/<?php echo $activity->magistrate_court_case_paper; ?>" width="100%" alt="FingurePrint"  class="img-rounded user_image_shadow user-img-center"> <?php
-                                                  }elseif($activity->enforcement_information == "Drug_Court"){
-                                                    ?> <img src="files/drug_court/<?php echo $activity->drug_court_case_paper; ?>" width="100%" alt="FingurePrint"  class="img-rounded user_image_shadow user-img-center"> <?php
-                                                  }else{
-                                                    echo "No Case Paper Uploaded!";
-                                                  }
-                                                  ?>
-                                                </li>
-                                                
+                                                <?php 
+                                                if($activity->enforcement_information == "Mobile_Court"){
+                                                ?> 
+                                                <br/><li> <strong>Mobile Court Information</strong></li>
+                                                <br/> 
+                                                <table>
+                                                    <tr>
+                                                        <td>Company name & address</td>
+                                                        <td>{{ $activity->mobile_court_company_name_address }}</td>
+                                                        <td>Pharmacy name & address</td>
+                                                        <td>{{ $activity->mobile_court_pharmacy_name_address }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>No. of cases filed</td>
+                                                        <td>{{ $activity->mobile_court_no_of_cases_filed }}</td>
+                                                        <td>No. of convicted person</td>
+                                                        <td>{{ $activity->mobile_court_no_of_convicted_person }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Fine amount</td>
+                                                        <td>{{ $activity->mobile_court_fine_amount }}</td>
+                                                        <td>No. of court jail</td>
+                                                        <td>{{ $activity->mobile_court_jail }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Value of seized medicine</td>
+                                                        <td colspan="3">{{ $activity->mobile_court_value_of_seized_medicine }}</td>
+                                                    </tr>
+                                                </table>
+                                                <img src="files/mobile_court/<?php echo $activity->mobile_court_case_paper; ?>" width="100%" alt="FingurePrint"  class="img-rounded user_image_shadow user-img-center"> 
+                                                <?php
+                                                }elseif($activity->enforcement_information == "Magistrate_Court"){
+                                                ?> 
+                                                <br/><li> <strong>Magistrate Court Information</strong></li><br/> 
+                                                <table>
+                                                    <tr>
+                                                        <td>Company name & address</td>
+                                                        <td>{{ $activity->magistrate_court_company_name_address }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>No. of cases filed</td>
+                                                        <td>{{ $activity->magistrate_court_no_of_case_filed }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Case no.</td>
+                                                        <td>{{ $activity->magistrate_court_case_no }}</td>
+                                                    </tr>
+                                                </table>
+                                                <img src="files/magistrate_court/<?php echo $activity->magistrate_court_case_paper; ?>" width="100%" alt="FingurePrint"  class="img-rounded user_image_shadow user-img-center"> 
+                                                <?php
+                                                }elseif($activity->enforcement_information == "Drug_Court"){
+                                                ?> 
+                                                <br/><li><strong>Drug Court Information</strong></li><br/> 
+                                                <table>
+                                                    <tr>
+                                                        <td>Company name & address</td>
+                                                        <td>{{ $activity->drug_court_company_name_address }}</td>
+                                                        <td>Pharmacy name & address</td>
+                                                        <td>{{ $activity->drug_court_pharmacy_name_address }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Substandard drug</td>
+                                                        <td>{{ $activity->drug_court_substandard_drug ? 'Yes' : 'No' }}</td>
+                                                        <td>Unregistered drug</td>
+                                                        <td>{{ $activity->drug_court_unregistered_drug ? 'Yes' : 'No'  }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Govt. medicine</td>
+                                                        <td>{{ $activity->drug_court_govt_medicine ? 'Yes' : 'No' }}</td>
+                                                        <td>Adulterated/spurious/misbranded</td>
+                                                        <td>{{ $activity->drug_court_adulterated_spurious_misbranded ? 'Yes' : 'No'  }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Unauthorized raw material</td>
+                                                        <td>{{ $activity->drug_court_unauthorized_raw_material ? 'Yes' : 'No' }}</td>
+                                                        <td>Over pricing</td>
+                                                        <td>{{ $activity->drug_court_over_pricing ? 'Yes' : 'No'  }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Illegal advertisement</td>
+                                                        <td>{{ $activity->drug_court_illegal_advertisement ? 'Yes' : 'No' }}</td>
+                                                        <td>Not registered</td>
+                                                        <td>{{ $activity->drug_court_not_registered ? 'Yes' : 'No'  }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Others</td>
+                                                        <td>{{ $activity->drug_court_others ? 'Yes' : 'No' }}</td>
+                                                        <td>Case no with date</td>
+                                                        <td>{{ $activity->drug_court_case_no_with_date }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Description</td>
+                                                        <td colspan="3">{{ $activity->drug_court_description }}</td>
+                                                    </tr>
+                                                </table>
+                                                <img src="files/drug_court/<?php echo $activity->drug_court_case_paper; ?>" width="100%" alt="FingurePrint"  class="img-rounded user_image_shadow user-img-center"> 
+                                                <?php
+                                                }else{
+                                                echo "No Case Paper Uploaded!";
+                                                }
+                                                ?>
+                                                <br/><br/><li> <strong>Official Activities</strong></li><br/>
+                                                <table>
+                                                    <tr>
+                                                        <td>No. of new drug licenses issued</td>
+                                                        <td>{{ $activity->official_no_of_new_drug_licenses_issued }}</td>
+                                                        <td>No. of new drug licenses calncelled</td>
+                                                        <td>{{ $activity->official_no_of_drug_licenses_calncelled }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>No. of drug licenses transfer in</td>
+                                                        <td>{{ $activity->official_no_of_drug_licenses_transfer_in }}</td>
+                                                        <td>No. of drug licenses transfer out</td>
+                                                        <td>{{ $activity->official_no_of_drug_licenses_transfer_out }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Total No. of drug license</td>
+                                                        <td>{{ $activity->official_total_no_of_drug_licenses }}</td>
+                                                        <td>No. of drug licenses renewed</td>
+                                                        <td>{{ $activity->official_no_of_drug_license_renewed }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>No. of drug licenses ownership transferred</td>
+                                                        <td>{{ $activity->official_no_of_drug_license_ownership_transferred }}</td>
+                                                        <td>No. of drug licenses address changed</td>
+                                                        <td>{{ $activity->official_no_of_drug_license_address_changed }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Total revenue receipt</td>
+                                                        <td>{{ $activity->official_total_revenue_receipt }}</td>
+                                                        <td>No. of sample sent</td>
+                                                        <td>{{ $activity->official_no_of_sample_sent }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>No. of test report received</td>
+                                                        <td>{{ $activity->official_no_of_test_report_received }}</td>
+                                                        <td>No. of substandard drugs</td>
+                                                        <td>{{ $activity->official_no_of_substandard_drugs }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Description of substandard drugs</td>
+                                                        <td colspan="3">{{ $activity->official_description_of_substandard_drugs }}</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Others</td>
+                                                        <td>{{ $activity->official_others }}</td>
+                                                        <td>Sealed</td>
+                                                        <td>{{ $activity->official_sealed }}</td>
+                                                    </tr>
+                                                </table>
                                               </ul>
                                             </div>
                                           </div>
